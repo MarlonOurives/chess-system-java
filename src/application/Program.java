a40567bdb864bc19d6f996f473f6b184a2067852
Marlon Ourives <marloncicero_araujo@hotmail.com>
2021-06-15 13:32:16 -0400

    Primeira classe Position

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,13 @@
 package application;
 
+import boardgame.Position;
+
 public class Program {
 
 	public static void main(String[] args) {
 
-		System.out.println("Init");
+		Position pos = new Position(3, 4);
+		System.out.println(pos);
 	}
 
 }
